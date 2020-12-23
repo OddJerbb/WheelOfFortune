7ab86bb3f08521890afa4c6483a9964f52f12860
Nicholas Huang <55514223+OddJerbb@users.noreply.github.com>
2020-12-23 14:34:00 -0600

    Update Round.java
    Update file for version 2

--- Round.java
+++ Round.java
@@ -1,192 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package indproject;
+package wheeloffortune;
 
 import java.io.*;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
-/**
- *
- * @author s561561
- */
 public class Round {
-    private static int cW;
+
+    private static int currentWedge;
 
     private static boolean flag;
     private static int iterator; //iterator
 
     private static Thread t1;
     private static Thread t2;
+    private static Thread t3;
 
-    private static Integer power;
     private static String letter;
-    private static String guesses;
+    private static String guesses; //Guessed letters
 
-    private static String[] bank;
+    private static String[] bank; //Available letters
 
-    private Random rand;
+    //Objects used to generate a puzzle
+    private static Random rand = new Random();
     private File puzzleF;
     private Scanner scan;
     private static String solution;
     private static String puzzle;
 
-    //private static final int[] wedges = {500, 900, 700, 300, 800, 550, 400, 500, 600,
-        //350, 500, 900, -1, 650, 500, 700, -2, 800, 500, 450, 500, 300, -1, 5000};
-    private static final int[] wedges = {500, 5000, -1, 300, 500, 450, 500, 800, -2, 
+    private static final int[] wedges = {500, 5000, -1, 300, 500, 450, 500, 800, -2,
         700, 500, 650, -1, 900, 500, 350, 600, 500, 400, 550, 800, 300, 700, 900};
 
     public Round() throws IOException {
-        guesses = "-& ";
+        guesses = "-&, ";
         bank = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
         choosePuzzle();
 
         iterator = 0;
         flag = true;
-        play(Start2.getList().get(0), 0);
-    }
-
-    public static void play(Player P, int currentWedge) throws IOException {
-        cW = currentWedge;
-
-        t1 = new Thread(new Runnable() {
-            public void run() {
-                new commandPrompt(P);
-            }
-        });
-
-        t2 = new Thread(new Runnable() {
-            public void run() {
-                //Chosen a vowel
-                if ("aeiou".contains(letter.toLowerCase()) && Arrays.asList(bank).contains(letter.toLowerCase()) && flag) {
-                    P.addRoundBalance(-200);
-                    GameWindow.updatePlayer(iterator);
-
-                    for (int x = 0; x < bank.length; x++) {
-                        if (bank[x].equalsIgnoreCase(letter)) {
-                            bank[x] = "*";
-                        }
-                    }
-                    GameWindow.updateLetters(bank);
-
-                    //Is part of the solution?
-                    if (solution.contains(letter.toUpperCase())) {
-                        guesses = guesses.concat(letter.toUpperCase());
-                        setPuzzle();
-
-                        try {
-                            play(P, currentWedge);
-                        } catch (IOException ex) {
-                            Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-
-                    } else {
-                        try {
-                            iterator += 1;
-                            play(Start2.getList().get(iterator %= 3), currentWedge);
-                        } catch (IOException ex) {
-                            Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }
-
-                    //Chosen a consonant
-                } else if (Arrays.asList(bank).contains(letter.toLowerCase()) && flag) {
-                    int newWedge = spin(currentWedge);
-
-                    if (wedges[newWedge] > 0) {
-                        for (int x = 0; x < bank.length; x++) {
-                            if (bank[x].equalsIgnoreCase(letter)) {
-                                bank[x] = "*";
-                            }
-                        }
-                        GameWindow.updateLetters(bank);
-
-                        if (solution.contains(letter.toUpperCase()) && !(guesses.contains(letter.toLowerCase()))) {
-                            guesses = guesses.concat(letter.toUpperCase());
-                            setPuzzle();
-                            P.addRoundBalance(wedges[newWedge]);
-                            GameWindow.updatePlayer(iterator);
-                            try {
-                                play(P, newWedge);
-                            } catch (IOException ex) {
-                                Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
-                            }
-
-                        } else {
-                            try {
-                                iterator += 1;
-                                play(Start2.getList().get(iterator %= 3), newWedge);
-                            } catch (IOException ex) {
-                                Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
-                            }
-                        }
-                    } else if (wedges[newWedge] == -1) {
-                        P.addRoundBalance(-(P.getRoundBalance()));
-                        GameWindow.updatePlayer(iterator);
-                        try {
-                            iterator += 1;
-                            play(Start2.getList().get(iterator %= 3), newWedge);
-                        } catch (IOException ex) {
-                            Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    } else if (wedges[newWedge] == -2) {
-                        try {
-                            iterator += 1;
-                            play(Start2.getList().get(iterator %= 3), newWedge);
-                        } catch (IOException ex) {
-                            Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }
-                } else if (flag) {
-                    try {
-                        iterator += 1;
-                        play(Start2.getList().get(iterator %= 3), currentWedge);
-                    } catch (IOException ex) {
-                        Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-            }
-        });
-
-        t1.start();
-    }
-
-    public static int spin(int wedge) {
-        int newWedge = (int) (wedge + power * .233 * 24);
-        
-        int change = Math.abs(newWedge - wedge);
-        
-        while (newWedge >= 24) {
-            newWedge = newWedge - 24;
-        }
-        
-        GameWindow.spinWheel(15 * change);
-        return newWedge;
+        play(Setup.getPlayers().get(0), 0);
     }
 
     public void choosePuzzle() throws IOException {
-        rand = new Random();
-
         switch (rand.nextInt(5)) {
             case 0:
-                puzzleF = new File("indproject/Puzzles/Around The House.txt");
+                puzzleF = new File("wheeloffortune/Puzzles/Around the House.txt");
                 break;
             case 1:
-                puzzleF = new File("indproject/Puzzles/Before and After.txt");
+                puzzleF = new File("wheeloffortune/Puzzles/Before and After.txt");
                 break;
             case 2:
-                puzzleF = new File("indproject/Puzzles/Characters.txt");
+                puzzleF = new File("wheeloffortune/Puzzles/Characters.txt");
                 break;
             case 3:
-                puzzleF = new File("indproject/Puzzles/Food And Drink.txt");
+                puzzleF = new File("wheeloffortune/Puzzles/Food and Drink.txt");
                 break;
             case 4:
-                puzzleF = new File("indproject/Puzzles/Phrase.txt");
+                puzzleF = new File("wheeloffortune/Puzzles/Phrase.txt");
                 break;
             default:
                 break;
@@ -214,77 +84,207 @@ public class Round {
             if (guesses.contains(Character.toString(solution.charAt(x)))) {
                 puzzle = puzzle.concat(Character.toString(solution.charAt(x)));
             } else {
-                puzzle = puzzle.concat(Character.toString('*'));
+                puzzle = puzzle.concat(Character.toString('#'));
             }
         }
         GameWindow.setPuzzle();
+    }
+
+    public static String getPuzzle() {
+        return puzzle;
+    }
+
+    public static void play(Player P, int cW) throws IOException {
+        currentWedge = cW;
+
+        t1 = new Thread(new Runnable() {
+            public void run() {
+                new CommandPrompt(P);
+            }
+        });
+
+        t2 = new Thread(new Runnable() {
+            public void run() {
+                //Player has played a vowel
+                if ("aeiou".contains(letter.toLowerCase()) && Arrays.asList(bank).contains(letter.toLowerCase()) && flag) {
+                    P.addRoundBalance(-250);
+                    GameWindow.updatePlayer(iterator);
+
+                    for (int x = 0; x < bank.length; x++) {
+                        if (bank[x].equalsIgnoreCase(letter)) {
+                            bank[x] = "#";
+                        }
+                    }
+                    GameWindow.updateLetters(bank);
+
+                    if (solution.contains(letter.toUpperCase())) {
+                        guesses = guesses.concat(letter.toUpperCase());
+                        setPuzzle();
+
+                        try {
+                            play(P, cW);
+                        } catch (IOException ex) {
+                            Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+
+                    } else {
+                        try {
+                            iterator += 1;
+                            play(Setup.getPlayers().get(iterator %= 3), cW);
+                        } catch (IOException ex) {
+                            Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                } //Player has chosen to spin the wheel
+                else {
+                    //Spin a new wedge!
+                    currentWedge = spin(cW);
+
+                    //Player has spun a Bankrupt!! or Lose a Turn! segment
+                    if (wedges[currentWedge] < 0) {
+                        //Bankrupt!!
+                        if (wedges[currentWedge] == -1) {
+                            P.clearRoundBalance();
+                            GameWindow.updatePlayer(iterator);
+                            try {
+                                iterator += 1;
+                                play(Setup.getPlayers().get(iterator %= 3), currentWedge);
+                            } catch (IOException ex) {
+                                Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        } //Lose a Turn!
+                        else if (wedges[currentWedge] == -2) {
+                            try {
+                                iterator += 1;
+                                play(Setup.getPlayers().get(iterator %= 3), currentWedge);
+                            } catch (IOException ex) {
+                                Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+                    } 
+
+                    //Player will now choose a consonant
+                    else {
+                        new CommandPrompt(P, currentWedge);
+                    }
+                }
+            }
+        });
+
+        t3 = new Thread(new Runnable() {
+            public void run() {
+                for (int x = 0; x < bank.length; x++) {
+                    if (bank[x].equalsIgnoreCase(letter)) {
+                        bank[x] = "#";
+                    }
+                }
+                GameWindow.updateLetters(bank);
+
+                if (solution.contains(letter.toUpperCase()) && !(guesses.contains(letter.toLowerCase()))) {
+                    guesses = guesses.concat(letter.toUpperCase());
+                    setPuzzle();
+                    P.addRoundBalance(wedges[currentWedge]);
+                    GameWindow.updatePlayer(iterator);
+                    try {
+                        play(P, currentWedge);
+                    } catch (IOException ex) {
+                        Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                } else {
+                    try {
+                        iterator += 1;
+                        play(Setup.getPlayers().get(iterator %= 3), currentWedge);
+                    } catch (IOException ex) {
+                        Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        });
+
+        t1.start();
+    }
+
+    public static int spin(int wedge) {
+        int newWedge = (int) (wedge + rand.nextInt(10) * .233 * 24);
+
+        int change = Math.abs(newWedge - wedge);
+
+        while (newWedge >= 24) {
+            newWedge = newWedge - 24;
+        }
+
+        GameWindow.spinWheel(15 * change);
+        return newWedge;
     }
 
     public static void clearBoard(Player p) {
         if (p.getRoundBalance() < 500) {
             p.addRoundBalance(500 - p.getRoundBalance());
             p.addBalance(p.getRoundBalance());
-            p.addRoundBalance(-(p.getRoundBalance()));
+            p.clearRoundBalance();
         } else {
             p.addBalance(p.getRoundBalance());
-            p.addRoundBalance(-(p.getRoundBalance()));
+            p.clearRoundBalance();
         }
 
-        if (p.getNum() == 1) {
-            Start2.getList().get(1).addRoundBalance(-(Start2.getList().get(1).getRoundBalance()));
-            Start2.getList().get(2).addRoundBalance(-(Start2.getList().get(2).getRoundBalance()));
-        } else if (p.getNum() == 2) {
-            Start2.getList().get(0).addRoundBalance(-(Start2.getList().get(0).getRoundBalance()));
-            Start2.getList().get(2).addRoundBalance(-(Start2.getList().get(2).getRoundBalance()));
-        } else {
-            Start2.getList().get(0).addRoundBalance(-(Start2.getList().get(0).getRoundBalance()));
-            Start2.getList().get(1).addRoundBalance(-(Start2.getList().get(1).getRoundBalance()));
+        switch (p.getNum()) {
+            case 1:
+                Setup.getPlayers().get(1).clearRoundBalance();
+                Setup.getPlayers().get(2).clearRoundBalance();
+                break;
+            case 2:
+                Setup.getPlayers().get(0).clearRoundBalance();
+                Setup.getPlayers().get(2).clearRoundBalance();
+                break;
+            case 3:
+                Setup.getPlayers().get(0).clearRoundBalance();
+                Setup.getPlayers().get(1).clearRoundBalance();
+                break;
+            default:
+                break;
         }
 
         GameWindow.updatePlayer(0);
         GameWindow.updatePlayer(1);
         GameWindow.updatePlayer(2);
     }
-    
+
     public static void resetBank() {
         bank = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
     }
-    
-    public static String[] getBank() {
-        return bank;
+
+    public static void endTurn() throws IOException {
+        iterator += 1;
+        play(Setup.getPlayers().get(iterator %= 3), currentWedge);
+    }
+
+    public static void setLetter(String L) {
+        letter = L;
+    }
+
+    public static void setIterator(int n) {
+        iterator = n;
+    }
+
+    public static Thread getThread2() {
+        return t2;
     }
     
-    public static String getPuzzle() {
-        return puzzle;
+    public static Thread getThread3() {
+        return t3;
     }
 
     public static String getSolution() {
         return solution;
     }
 
-    public static void setPower(Integer i) {
-        power = i;
+    public static String[] getBank() {
+        return bank;
     }
 
-    public static void setLetter(String s) {
-        letter = s;
-    }
-
-    public static Thread getThread() {
-        return t2;
-    }
-
-    public static void setFlag(boolean b) {
-        flag = b;
-    }
-
-    public static void endTurn() {
-        try {
-            iterator += 1;
-            play(Start2.getList().get(iterator %= 3), cW);
-        } catch (IOException ex) {
-            Logger.getLogger(Round.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public static int getCurrentWedge() {
+        return wedges[currentWedge];
     }
 }
